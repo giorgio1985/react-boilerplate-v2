470557e0d2d385aa2822c9141cdf8d5699fd2d01
giorgio1985 <g.adonoo@yahoo.it>
2020-10-02 15:45:13 +0200

    aggiornamento ...

--- app/components/ModalDialog/index.js
+++ app/components/ModalDialog/index.js
@@ -1,9 +1,3 @@
-/**
- *
- * ModalDialog
- *
- */
-
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
--- app/containers/Contact/index.js
+++ app/containers/Contact/index.js
@@ -17,28 +17,37 @@ import saga from './saga';
 import gmap from '../../images/gmap.jpg';
 import ZoomOutMapIcon from '@material-ui/icons/ZoomOutMap';
 import './contact.css';
-//import  ModalDialog  from '../../components/ModalDialog';
+import  ModalDialog  from '../../components/ModalDialog';
 
 
-export function Contact() {
+export function Contact() {    {/* Nome del componente  */}
+
   useInjectReducer({ key: 'contact', reducer });
   useInjectSaga({ key: 'contact', saga });
 
-
-  const zoomIcon = () => {
-
-   return (
-    alert('Coming soon!')
-)
-
-  }
+  
+  const ZoomIcon = () => {  {/* nome della funzione deputata alla view della dialog*/}
+  console.log('Coming Soon!')
+    return (
+    <div className="modalDialog">
+         <ModalDialog
+         
+         id = '1'
+         commento = 'commento 1'
+         
+         />
+     </div>
+ )
+   }
 
   return (
 <div>
+
+  
    <div className="googleMap">
       <img src={gmap}  alt="google map"  style={{width: '100%'}}/>  
-      <div className="zoomMapIcon" onClick={zoomIcon}>
-      <ZoomOutMapIcon/>
+    <div className="zoomMapIcon">
+      <ZoomIcon/>
    </div>
 </div>
 <div className="descriptionMap">
